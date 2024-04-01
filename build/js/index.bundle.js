31151a4c9766f6ba9578ccfd3fd059735a1c48b5
konstantin-86 <yakimtsev.konstantin@gmail.com>
2024-04-01 18:28:04 +0300

    1st day

---
+++ build/js/index.bundle.js
@@ -0,0 +1,1 @@
+new Swiper(".mySlider1",{slidesPerView:4,navigation:{nextEl:".mySlider1-next",prevEl:".mySlider1-prev"}}),new Swiper(".ourWork-Slider",{slidesPerView:1,spaceBeetwen:0,navigation:{nextEl:".ourWork-next",prevEl:".ourWork-prev"},breakpoints:{1600:{slidesPerView:5},1366:{slidesPerView:4},768:{slidesPerView:3},480:{slidesPerView:2}}});
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-
+const swiper = new Swiper(".mySlider1", {
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".mySlider1-next",
+    prevEl: ".mySlider1-prev",
+  },
+});
+const swiperSecond = new Swiper(".ourWork-Slider", {
+  slidesPerView: 1,
+  spaceBeetwen: 0,
+  navigation: {
+    nextEl: ".ourWork-next",
+    prevEl: ".ourWork-prev",
+  },
+  breakpoints: {
+    1600: {
+      slidesPerView: 5,
+    },
+    1366: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+  },
+});
